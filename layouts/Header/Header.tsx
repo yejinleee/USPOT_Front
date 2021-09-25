3419015c8c20c22171d244c4bfa71f208bcb4804
jeongjiyeon315 <jung_j_yeon@naver.com>
2021-09-25 20:40:39 +0900

    이미지 경로  변경

--- layouts/Header/Header.tsx
+++ layouts/Header/Header.tsx
@@ -8,19 +8,19 @@ class Header extends Component {
       <header className="Header">
         <div className="Contents">
           <Link to="/" style={{ textDecoration: 'none', color: '#000000' }}>
-            <img id="logo" src="src/icon/USPOT로고.png" alt="USPOT" width="120" height="90" />
+            <img id="logo" src="/src/icon/USPOT로고.png" alt="USPOT" width="120" height="90" />
           </Link>
           <nav className="Navigation">
             <ul>
               <li>
                 <Link to="/" style={{ textDecoration: 'none', color: '#000000' }}>
-                  <img id="home" src="src/icon/home.png" alt="home" width="30" height="30" />
+                  <img id="home" src="/src/icon/home.png" alt="home" width="30" height="30" />
                   HOME
                 </Link>
               </li>
               <li>
                 <Link to="/mypage" style={{ textDecoration: 'none', color: '#000000' }}>
-                  <img id="mypage" src="src/icon/mypage.png" alt="mypage" width="30" height="30" />
+                  <img id="mypage" src="/src/icon/mypage.png" alt="mypage" width="30" height="30" />
                   MYPAGE
                 </Link>
               </li>
--- pages/Home/Mainhome.tsx
+++ pages/Home/Mainhome.tsx
@@ -1,14 +1,11 @@
-import Search from '@components/Search/Search';
-import Toggle from '@components/Toggle/Toggle';
 import React, { useEffect, useState } from 'react';
+import Toggle from '@components/Toggle/Toggle';
 import Layouts from '@layouts/Layouts';
 import Second from '@pages/Secondpage/Second';
 import KakaoMap from '@components/Map/KaKaomap';
-import axios from 'axios';
 
 const HomePage = () => (
   <Layouts>
-    {/* <Search /> */}
     <Toggle />
     {/* <KakaoMap /> */}
     {/*<KakaoMap />*/}
