b5a34f9db51b8f52583aad82fe105bfc5df6a345
jeongjiyeon315 <jung_j_yeon@naver.com>
2021-10-07 20:26:40 +0900

    env 키 업로드

--- pages/LogIn/Sample.tsx
+++ pages/LogIn/Sample.tsx
@@ -10,7 +10,7 @@ const loginWithKaKao = () => {
 };
 
 const Sample = () => {
-  const REST_API_KEY = '9019da36b0a192d988f66c0cd0ec27c8';
+  const REST_API_KEY = process.env.REACT_APP_KAKAO_KEY;
   const REDIRECT_URI = 'http://localhost:8090/oauth/callback/kakao';
 
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
