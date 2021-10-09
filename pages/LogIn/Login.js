ee0c5405b0a75668e6b2f3ecd52047bd62bc2d84
yejin9487@gmail.com <yejin9487@gmail.com>
2021-10-09 20:47:27 +0900

    즐겨찾기 top5 수정완

---
+++ pages/LogIn/Login.js
@@ -0,0 +1,11 @@
+// // import { KAKAO_AUTH_URL } from "../shared/OAuth";
+// import {KAKAO_AUTH_URL} from './OAuth';
+//
+// const kklogin =()=> {
+//   <KaKaoBtn href={KAKAO_AUTH_URL}>
+//     {/*<img src={kakaologo}></img>*/}
+//     <span>카카오계정 로그인</span>
+//   </KaKaoBtn>
+// }
+//
+// export default kklogin;
---
+++ pages/LogIn/OAuth.js
@@ -0,0 +1,4 @@
+// const CLIENT_ID = "1aefd456416cde07f851f05dc5f2ff6f";
+// const REDIRECT_URI =  "http://localhost:8090/oauth/callback/kakao";
+//
+// export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
