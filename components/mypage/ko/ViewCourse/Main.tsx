03fcc01c9f984cb48678c43eced947447544e7f9
jeongjiyeon315 <jung_j_yeon@naver.com>
2021-10-31 03:19:27 +0900

    myplace en 버전 수정

--- components/mypage/ko/ViewCourse/Main.tsx
+++ components/mypage/ko/ViewCourse/Main.tsx
@@ -3,16 +3,6 @@ import CourseList from './CourseList';
 import '@components/mypage/ViewCourse.scss';
 
 const ViewMain = () => {
-  // const courselist: any = course.map((v: string, index: number) => (
-  //   <>
-  //     <div className="viewbuttonclass">
-  //       <button className="deletebutton" key={index} onClick={() => onClick(course[index].courseid)}>
-  //         <p className="deletename">{course[index].name}</p>
-  //       </button>
-  //     </div>
-  //   </>
-  // ));
-
   return (
     <>
       <h3 className="viewtitle">코스 한눈에 보기</h3>
