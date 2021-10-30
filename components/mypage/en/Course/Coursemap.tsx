31c77c2b947d7a144b426ee088eb426a5ead7dd7
jeongjiyeon315 <jung_j_yeon@naver.com>
2021-10-30 16:34:19 +0900

    myplace 아이콘 변경 + myplace en 시작 지점 설정

--- components/mypage/en/Course/Coursemap.tsx
+++ components/mypage/en/Course/Coursemap.tsx
@@ -1,84 +1,69 @@
 /*global kakao */
 import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { FC, useCallback, useEffect, useRef, useState } from 'react';
 
-export default function Coursemap() {
-  const latt = useRef(0);
-  const long = useRef(0);
+const EnCoursemap = () => {
   const kakao = (window as any).kakao;
-  var local = sessionStorage.getItem('memberid');
-  try {
-    var memberid = Number(local.split('')[1]);
-  } catch {
-    var memberid = 0;
-  }
-  const [myplace, setMyplace] = useState([] as any);
+  const [map, setMap] = useState(null);
+  const [station, setStation] = useState([] as any);
+  const [start, setStart] = useState();
+  const x = useRef(0);
+  const y = useRef(0);
 
   useEffect(() => {
-    axios
-      .get(`/api/myplace/findall/${memberid}`)
-      .then(async (response) => {
-        setMyplace(response.data.data);
-      })
-      .catch((error) => {});
+    const container = document.getElementById('myplacemap');
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 13,
+    };
+    setMap(new kakao.maps.Map(container, options));
   }, []);
 
   useEffect(() => {
-    mapscript();
-  }, [myplace]);
-
-  const mapscript = () => {
-    myplace.forEach((el: any) => {
-      latt.current += el.location_y;
-      long.current += el.location_x;
+    axios.get('/api/en/station/findall').then((response) => {
+      setStation(response.data.data);
     });
+  }, []);
 
-    let container = document.getElementById('myplacemap');
-    let options = {
-      center: new kakao.maps.LatLng(latt.current / myplace.length, long.current / myplace.length),
-      level: 13,
-    };
+  function displayMarker(place: any) {
+    console.log(place);
 
-    const myplacemap = new kakao.maps.Map(container, options);
-
-    myplace.forEach((el: any) => {
-      var imageSize = new kakao.maps.Size(36, 37),
-        imageOption = { offset: new kakao.maps.Point(27, 69) };
-      if (el.category === '관광명소') {
-        var id = 1;
-      } else if (el.category === '식당') {
-        var id = 2;
-      } else {
-        var id = 3;
-      }
-      var imageSrc = `/src/icon/${id}.png`;
-      const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-      const marker = new kakao.maps.Marker({
-        position: new kakao.maps.LatLng(el.location_y, el.location_x),
-        image: markerImage,
-      });
-
-      marker.setMap(myplacemap);
-      var infowindow = new kakao.maps.InfoWindow({
-        content: el.name,
-      });
-
-      kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(myplacemap, marker, infowindow));
-      kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+    let marker = new kakao.maps.Marker({
+      map: map,
+      position: new kakao.maps.LatLng(place.location_y, place.location_x),
     });
+    kakao.maps.event.addListener(marker, 'mouseover', function () {
+      infowindow.open(map, marker);
+      infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.name + '</div>');
+    });
+    kakao.maps.event.addListener(marker, 'mouseout', function () {
+      infowindow.close();
+    });
+    kakao.maps.event.addListener(marker, 'click', function () {
+      infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.name + '</div>');
+      infowindow.open(map, marker);
+    });
+  }
+  if (station.length !== 0) {
+    console.log(station);
+    var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+    x.current = 0;
+    y.current = 0;
 
-    function makeOverListener(map: any, marker: any, infowindow: { open: (arg0: any, arg1: any) => void }) {
-      return function () {
-        infowindow.open(map, marker);
-      };
+    for (var i = 0; i < station.length; i++) {
+      x.current += station[i].location_x;
+      y.current += station[i].location_y;
     }
 
-    function makeOutListener(infowindow: { close: () => void }) {
-      return function () {
-        infowindow.close();
-      };
+    var Position = new kakao.maps.LatLng(y.current / station.length, x.current / station.length);
+
+    map.setCenter(Position);
+    for (let i = 0; i < station.length; i++) {
+      displayMarker(station[i]);
     }
-  };
+  }
 
   return <div id="myplacemap" style={{ width: '50vw', height: '50vw' }}></div>;
-}
+};
+
+export default EnCoursemap;
