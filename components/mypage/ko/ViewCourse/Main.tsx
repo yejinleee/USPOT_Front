29ce3e3bdb5858cd5ac4246ef2e4df8f90cc88c3
yejin9487@gmail.com <yejin9487@gmail.com>
2021-11-01 12:10:11 +0900

    1페이지버튼수정, 뷰코스 멘트수정

--- components/mypage/ko/ViewCourse/Main.tsx
+++ components/mypage/ko/ViewCourse/Main.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CourseList from './CourseList';
 import '@components/mypage/ViewCourse.scss';
+import axios from 'axios';
+import Main from '@components/mypage/ko/ReviseCourse/Main';
+import None from '@components/mypage/ko/ReviseCourse/None';
 
 const ViewMain = () => {
+  var local = sessionStorage.getItem('memberid');
+  try {
+    var memberid = Number(local.split('')[1]);
+  } catch {
+    var memberid = 0;
+  }
+  const [exist, setExsist] = useState(false);
+  useEffect(() => {
+    axios
+      .get(`/api/course/findall/${memberid}`)
+      .then(async (response) => {
+        if (response.data.data.length !== 0) {
+          setExsist(true);
+        }
+      })
+      .catch((error) => {});
+  }, []);
+
   return (
     <>
-      <div className="revisetitle" style={{margin:'2vh 0 1vh'}}>확인할 코스를 선택해주세요</div>
+      {exist ?
+        <>
+          <div className="revisetitle" style={{ margin: '2vh 0 1vh' }}>확인할 코스를 선택해주세요</div>
+          <CourseList />
+        </>
+        : <None />
+      }
 
-      <CourseList />
     </>
   );
 };
